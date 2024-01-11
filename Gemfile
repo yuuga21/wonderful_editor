124b88e9cc182e84c41f4e9c40da44986cdfaef7
yuga <yuga@example.com>
2024-01-11 22:17:17 +0900

    ER図に書いた各種テーブルとmodelの作成

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :development do
   gem "web-console", ">= 4.1.0"
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem "annotate"
+  gem "rails-erd"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
