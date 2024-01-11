70c15edafcf45cbd9a85535f8252095efe8746a0
yuga <yuga@example.com>
2024-01-11 21:49:10 +0900

    application.rb の設定

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,20 @@ module WonderfulEditor
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+    end
+
+    config.api_only = true
   end
 end
