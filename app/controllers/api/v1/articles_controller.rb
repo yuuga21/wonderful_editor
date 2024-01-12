3df882a6657707b2f848f48f037ff364698c73e6
yuga <yuga@example.com>
2024-01-12 23:43:54 +0900

    記事削除の実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -22,6 +22,11 @@ module Api::V1
       render json: article, serializer: Api::V1::ArticleSerializer
     end
 
+    def destroy
+      article = current_user.articles.find(params[:id])
+      article.destroy!
+    end
+
     private
 
       def article_params
